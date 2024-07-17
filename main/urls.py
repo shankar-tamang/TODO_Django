d7039eeca48b2492158f555c65d0a3aaf5c9220e
Shank Tamang <alwaysshankartamang@gmail.com>
2024-07-17 22:22:53 +0545

    initiated

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('add-blog/', views.add_blog, name='add-blog'),
+
+
+]
---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from main.models import TODO
+
+# Create your views here.
+
+def home(request):
+    todos = TODO.objects.all()
+    print(todos)
+    return render(request, 'main/home.html', {'todos':todos})
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def add_blog(request):
+
+    if request.method == 'GET':
+        return render(request, 'main/add_blog.html')
+        # request.method
+    else:
+        title = request.POST['title']  
+        desc = request.POST['description']
+        TODO.objects.create(title=title, content=desc, is_completed=False, user_id=1)
+        return redirect('http://127.0.0.1:8000/')
+
+
+
+
